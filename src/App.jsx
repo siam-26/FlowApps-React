04517d7837edf72c7b897fd3d898a39d9e2dc8a5
siam-26 <siamshahriar390@gmail.com>
2025-12-15 14:29:42 +0600

    navbar component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Pages/Shared_pages/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='max-w-[1440px] mx-auto'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
